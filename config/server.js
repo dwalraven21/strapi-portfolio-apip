a8a72c693866f6bd2c8f790860ca6752bc06f97f
Danielle Walraven <dwalraven21@gmail.com>
2020-09-21 09:57:02 -0400

    Set up backend.

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1338),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '52551923dcc18dbf9ef18b547767fe07'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '639b0c71-37ae-46cd-ade4-51b7a3eb032f'
+};
